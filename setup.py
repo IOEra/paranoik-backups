6ff3a0acb454423223a2ccc0d9eebc0ec588ad1f
Jordan Jambazov <jordan.jambazov@gmail.com>
2014-10-12 23:50:20 +0300

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name="Paranoik",
+    version="0.0.1",
+    author="Jordan Jambazov",
+    author_email="jordan.jambazov@gmail.com",
+    description=("An demonstration of how to create, document, and publish "
+                 "to the cheese shop a5 pypi.org."),
+    license="BSD",
+    keywords="backup",
+    url="https://github.com/jordanjambazov/paranoik-backups",
+    packages=['paranoik'],
+)
