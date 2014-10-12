d037566af37bfd2bf2db8d3fb3001d8d811b1999
Jordan Jambazov <jordan.jambazov@gmail.com>
2014-10-13 00:32:26 +0300

    Add providers module to setup file

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     version="0.0.1",
     author="Jordan Jambazov",
     author_email="jordan.jambazov@gmail.com",
-    license="BSD",
     keywords="backup",
     url="https://github.com/jordanjambazov/paranoik-backups",
-    packages=['paranoik.backup', 'paranoik.compress', 'paranoik.syncers', 'paranoik.utils'],
+    packages=['paranoik.backup', 'paranoik.backup.providers',
+              'paranoik.compress', 'paranoik.syncers', 'paranoik.utils'],
 )
