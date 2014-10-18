289af0524e1ba392aa15a467dc04101e702adabc
Jordan Jambazov <jordan.jambazov@gmail.com>
2014-10-19 01:51:33 +0300

    Add tests for s3

---
+++ paranoik/tests/s3_tests.py
@@ -0,0 +1,23 @@
+import unittest
+import mock
+
+from paranoik.syncers.s3 import S3
+
+
+class S3Tests(unittest.TestCase):
+    @mock.patch('paranoik.syncers.s3.Key')
+    @mock.patch('paranoik.syncers.s3.S3Connection')
+    def test_s3_valid_bucket(self, s):
+        s3 = S3('access_key', 'private_key')
+        s3.bucket = 'bucket_name'
+        s3.add_file('/path/to/some/file')
+
+    @mock.patch('paranoik.syncers.s3.Key')
+    @mock.patch('paranoik.syncers.s3.S3Connection')
+    def test_s3_valid_bucket(self, s3_connection, key):
+        s3 = S3('access_key', 'private_key')
+        self.assertRaises(ValueError, lambda: s3.add_file('/path/to/file'))
+
+
+if __name__ == "__main__":
+    unittest.main()
