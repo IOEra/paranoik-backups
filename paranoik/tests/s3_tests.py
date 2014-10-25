4edaa7a54e5ac099e3b5d1c9bae7491bc928e946
Jordan Jambazov <jordan.jambazov@gmail.com>
2014-10-26 02:48:31 +0300

    Change S3 tests to assert patched classes have been called.

--- paranoik/tests/s3_tests.py
+++ paranoik/tests/s3_tests.py
@@ -7,17 +7,18 @@ from paranoik.storage.s3 import S3
 class S3Tests(unittest.TestCase):
     @mock.patch('paranoik.storage.s3.Key')
     @mock.patch('paranoik.storage.s3.S3Connection')
-    def test_s3_valid_bucket(self, s):
+    def test_s3_valid_bucket(self, s3_connection, key):
         s3 = S3('access_key', 'private_key')
         s3.bucket = 'bucket_name'
         s3.add_file('/path/to/some/file')
+        self.assertTrue(s3_connection.called)
+        self.assertTrue(key.called)
 
-    @mock.patch('paranoik.storage.s3.Key')
     @mock.patch('paranoik.storage.s3.S3Connection')
-    def test_s3_valid_bucket(self, s3_connection, key):
+    def test_s3_no_valid_bucket(self, s3_connection):
         s3 = S3('access_key', 'private_key')
         self.assertRaises(ValueError, lambda: s3.add_file('/path/to/file'))
-
+        self.assertTrue(s3_connection.called)
 
 if __name__ == "__main__":
     unittest.main()
