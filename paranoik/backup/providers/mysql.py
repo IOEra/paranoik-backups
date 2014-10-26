a5f9cb388559855141628858c3e740b95f0c0d54
Jordan Jambazov <jordan.jambazov@gmail.com>
2014-10-26 03:53:45 +0300

    Add docstrings to MySQL provider.

--- paranoik/backup/providers/mysql.py
+++ paranoik/backup/providers/mysql.py
@@ -1,3 +1,8 @@
+"""
+Contains MySQL Backup Provider.
+"""
+
+
 import os
 
 from paranoik.backup.backupable import Backupable
@@ -5,41 +10,77 @@ from paranoik.utils.command_executor import CommandExecutor
 
 
 class MySQL(Backupable):
+    """
+    MySQL Backup Provider. Performs backup on a MySQL database.
+    """
+
     _database = None
     _username = None
     _password = None
 
     @property
     def database(self):
+        """
+        Getter of the database name.
+        :return:
+        """
         if self._database is None:
             raise ValueError("Database is not set.")
         return self._database
 
     @database.setter
     def database(self, value):
+        """
+        Setter of the database name.
+        :param value:
+        :return:
+        """
         self._database = value
 
     @property
     def username(self):
+        """
+        Getter of the username.
+        :return:
+        """
         if self._username is None:
             raise ValueError("Username is not set.")
         return self._username
 
     @username.setter
-    def username(self, value):
-        self._username = value
+    def username(self, username):
+        """
+        Setter of the username
+        :param username:
+        :return:
+        """
+        self._username = username
 
     @property
     def password(self):
+        """
+        Getter of the password.
+        :return:
+        """
         if self._password is None:
             raise ValueError("Password is not set.")
         return self._password
 
     @password.setter
     def password(self, value):
+        """
+        Setter of the password.
+        :param value:
+        :return:
+        """
         self._password = value
 
     def backup(self):
+        """
+        Executes mysqldump and performs backup. The backup is stored in the
+        destination file.
+        :return:
+        """
         with open(self.destination, 'w') as dump_file:
             command = ["mysqldump", "-u", self.username, "-p" + self.password,
                        self.database]
@@ -47,4 +88,8 @@ class MySQL(Backupable):
             executor.execute()
 
     def cleanup(self):
+        """
+        Removes the destination file.
+        :return:
+        """
         os.remove(self.destination)
