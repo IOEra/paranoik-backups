704c4342473e84dea930abe63e83be6faa5d8c35
Jordan Jambazov <jordan.jambazov@codeideo.com>
2016-02-19 17:16:41 +0200

    Increment minor version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ EXCLUDE_FROM_PACKAGES = ['paranoik.tests']
 
 setup(
     name="Paranoik",
-    version="0.0.8",
+    version="0.0.9",
     author="Jordan Jambazov",
     author_email="jordan.jambazov@codeideo.com",
     keywords="backup",
